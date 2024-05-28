beb5593650998222fc328b3e4e0c263a1aca58cb
Akash Shah <44998751+itsalfredakku@users.noreply.github.com>
2024-05-29 05:10:36 +0530

    Update

--- Client/Pages/Admin/Adboards/Adboards.razor.cs
+++ Client/Pages/Admin/Adboards/Adboards.razor.cs
@@ -41,18 +41,67 @@ namespace DOOH.Client.Pages.Admin.Adboards
         protected int adboardsCount;
 
 
+        protected bool isAdboardsLoading = false;
+
+
         protected async Task adboardsLoadData(LoadDataArgs args)
         {
             try
             {
-                var result = await DOOHDBService.GetAdboards(top: args.Top, skip: args.Skip, count:args.Top != null && args.Skip != null, filter: args.Filter, orderby: args.OrderBy);
+                isAdboardsLoading = true;
+
+                var result = await DOOHDBService.GetAdboards(top: args.Top, skip: args.Skip, count: args.Top != null && args.Skip != null, filter: args.Filter, orderby: args.OrderBy, expand: "Category");
 
                 adboards = result.Value.AsODataEnumerable();
+
+                foreach (var adboard in adboards)
+                {
+                    await Console.Out.WriteLineAsync(adboard.Category.CategoryColor);
+                }
+
                 adboardsCount = result.Count;
             }
             catch (Exception)
             {
                 NotificationService.Notify(new NotificationMessage { Severity = NotificationSeverity.Error, Summary = "Error", Detail = "Unable to load" });
+            }
+            finally
+            {
+                isAdboardsLoading = false;
+            }
+        }
+
+        // DetailsSplitButtonClicked
+        protected async void DetailsSplitButtonClicked(RadzenSplitButtonItem item, DOOH.Server.Models.DOOHDB.Adboard adboard)
+        {
+            if (item.Text == "Edit")
+            {
+                var dialogResult = await DialogService.OpenAsync<EditAdboard>("Edit Adboard", new Dictionary<string, object>() { { "AdboardId", adboard.AdboardId } });
+                if (dialogResult != null)
+                {
+                    await adboardsLoadData(new LoadDataArgs() { });
+                    StateHasChanged();
+                }
+            }
+            else if (item.Text == "Delete")
+            {
+                var dialogResult = await DialogService.Confirm("Are you sure?", "Delete Adboard");
+                if (dialogResult == true)
+                {
+                    try
+                    {
+                        await DOOHDBService.DeleteAdboard(adboardId: adboard.AdboardId);
+                        await adboardsLoadData(new LoadDataArgs() { });
+                    }
+                    catch (Exception)
+                    {
+                        NotificationService.Notify(new NotificationMessage { Severity = NotificationSeverity.Error, Summary = "Error", Detail = "Unable to delete" });
+                    }
+                    finally
+                    {
+                        StateHasChanged();
+                    }
+                }
             }
         }
     }
