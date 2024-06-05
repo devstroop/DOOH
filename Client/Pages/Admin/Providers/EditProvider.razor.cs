dba39f7406b1bb8c400b5e690969b9d633545e1e
Akash Shah <44998751+itsalfredakku@users.noreply.github.com>
2024-06-06 00:46:41 +0530

    Update

--- Client/Pages/Admin/Providers/EditProvider.razor.cs
+++ Client/Pages/Admin/Providers/EditProvider.razor.cs
@@ -8,7 +8,7 @@ using Microsoft.AspNetCore.Components.Web;
 using Radzen;
 using Radzen.Blazor;
 
-namespace DOOH.Client.Pages
+namespace DOOH.Client.Pages.Admin.Providers
 {
     public partial class EditProvider
     {
